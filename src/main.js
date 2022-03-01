1d8ca2ac63c537acce5f1d2b87c267a758ea31da
Maciej Kick <86595485+V3D1@users.noreply.github.com>
2022-03-01 09:52:20 +0100

    Revert "Add a couple vue apps"
    This reverts commit 36c1d020a4c2a4535b44b02fbabc5ef77f582388.

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import "../node_modules/bootstrap5/src/css/bootstrap.min.css";
-import "../node_modules/bootstrap5/src/js/bootstrap.bundle.min.js";
-
-createApp(App).use(store).use(router).mount("#app");
